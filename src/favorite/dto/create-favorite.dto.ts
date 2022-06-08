c47a2f88c9de3346c24bd486262d41f72eebacae
Jamile Boffo <jamileboffo@gmail.com>
2022-06-07 21:57:53 -0300

    Alterando Dto

--- src/favorite/dto/create-favorite.dto.ts
+++ src/favorite/dto/create-favorite.dto.ts
@@ -1,8 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsUUID } from "class-validator";
+import { Profile } from "src/profile/entities/profile.entity";
 
 
 export class CreateFavoriteDto {
+  @IsUUID(undefined, { each: true })
+  @ApiProperty({
+    description: "Perfil"
+  })
+  profile: Profile[];
+
   @IsUUID(undefined, { each: true })
   @ApiProperty({
     description: "Lista com os IDs dos jogos"
